1b6d037028a397d705d93e06c244092530e7207d
Lincoln Lopes <lincolnlopes@msn.com>
2021-01-24 02:44:42 -0300

    Resquest

--- frontend/src/components/Issue/IssueList.js
+++ frontend/src/components/Issue/IssueList.js
@@ -1,27 +1,40 @@
 import React from 'react'
 //import BootstrapTable from 'react-bootstrap-table-next';
 import { DataGrid } from '@material-ui/data-grid';
+import { ISSUE_GET } from '../../api';
 
-const rows = [
-  {
-    id: 1,
-    username: 'defunkt',
-    age: 38,
-  },
-];
+
+
+//const rows = [];
 
 
 const IssueList = () => {
+
+  const [data, setData] = React.useState([]);
+
+
+  async function go() {
+    const token = window.localStorage.getItem('token');
+    const { url, options } = ISSUE_GET(token);
+    const response = await fetch(url, options);
+    const data = await response.json();
+    setData(data);
+
+  }
+
+  go();
 
   return (
     <div style={{ height: 350, width: '100%' }}>
       <DataGrid
         columns={[
-          { field: 'id' },
-          { field: 'username', width: 200 },
-          { field: 'age' },
+          { field: 'id' , headerName: 'ID'},
+          { field: 'summary', headerName: 'Título' , width: 200 },
+          { field: 'description', headerName: 'Descrição' , width: 200 },
+          { field: 'meeting_id' , headerName: 'Reunião ID', width: 200 },
+
         ]}
-        rows={rows}
+        rows={data}
       />
     </div>
   )
